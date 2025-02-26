97f96a79f983910d873533d8c9bc42684f8cc78e
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-02-26 14:53:37 +0100

    added a paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <p></p>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
