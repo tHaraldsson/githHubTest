748fdd3f952d9110fe41463442087d5da98c17d3
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-02-26 15:09:30 +0100

    12345

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        
-      </div>
+      <div></div>
       <p></p>
       <div>bajs</div>
       <div>kiss</div>
@@ -28,7 +26,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
